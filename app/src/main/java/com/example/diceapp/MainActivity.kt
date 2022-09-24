d1434a452061f6bf45a363ce908e881491ae4a8c
HecatesRevenge <edgaramelia09@gmail.com>
2022-09-24 01:02:56 -0400

    Two Six Sided Die With Images

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         // find the button in the layout
         val rollButton: Button = findViewById(R.id.button)
         // This sets the value of the button to the interactive button found in activity_main//
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice();rollDice0()}
 
         // Do a dice roll on start-up
         rollDice()
@@ -25,6 +25,34 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView2)
         // Updates screen with the image for dice roll that display #2
+
+
+
+        /* Uses control flow if/else statements to display corresponding
+         side of dice when pressing the roll button
+         */
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        diceImage.setImageResource((drawableResource))
+        // Sets content description of dice image
+        diceImage.contentDescription = diceRoll.toString()
+    }
+    private fun rollDice0() {
+        //creates a dice with 6 sides and rolls it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        // Updates screen with the image for dice roll that display #2
+
+
 
         /* Uses control flow if/else statements to display corresponding
          side of dice when pressing the roll button
@@ -51,6 +79,13 @@ class MainActivity : AppCompatActivity() {
             return (1..numSides).random()
         }
     }
+    class Dice0(private  val numSides: Int){
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+
 
 
 }
